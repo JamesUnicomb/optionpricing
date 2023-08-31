a0c093c5cbcea5b2247644f8a525ae7cd1c71ad6
James Unicomb <james.unicomb@advancednavigation.com>
2023-09-01 09:47:38 +1000

    remove files

--- parallel.py
+++
@@ -1,8 +0,0 @@
-import optionpricing
-import numpy as np
-from timeit import timeit
-
-
-bm1 = optionpricing.BinomialMesh1D(48, 0.0)
-bm1.set_initial_condition()
-bm1.calculate_parallel()
--- serial.py
+++
@@ -1,9 +0,0 @@
-import optionpricing
-import numpy as np
-from timeit import timeit
-
-
-
-bm2 = optionpricing.BinomialMesh1D(48, 0.0)
-bm2.set_initial_condition()
-bm2.calculate_serial()
